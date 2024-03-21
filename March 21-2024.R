3c7b6252255ea80863b3a02e856d18171444cbd0
Ky-Mani Miller <156713196+KMiller06@users.noreply.github.com>
2024-03-21 10:05:42 -0400

    Add files via upload

---
+++ March 21-2024.R
@@ -0,0 +1,22 @@
+#Ky-Mani Miller Date:3/21/2024 
+#Paired T Test Pratice
+
+
+
+
+x=rnorm(10)
+y=rnorm(10)
+ttest=t.test(x,y)
+
+
+
+        Welch Two Sample t-test
+
+data:  x and y
+t = -0.63346, df = 17.338, p-value = 0.5347
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -1.2755891  0.6858134
+sample estimates:
+mean of x mean of y 
+0.2707194 0.5656073 
